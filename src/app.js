9457e633b72b6da774676b65350363fbdff08219
Marcos Fraga <marcosjsfraga@gmail.com>
2020-06-16 17:39:57 -0300

    Changes

---
+++ src/app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const cors = require("cors");
+const { uuid, isUuid } = require('uuidv4');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+//List
+app.get("/repositories", (request, response) => {
+	return response.json(repositories);
+});
+
+// Create
+app.post("/repositories", (request, response) => {
+	const { title, url, techs }  = request.body;
+
+	const repository = { 
+		id: uuid(), 
+		title, 
+		url, 
+		techs,
+		likes: 0 
+	};
+
+	repositories.push( repository );
+
+	return response.json(repository);
+});
+
+// Update
+app.put("/repositories/:id", (request, response) => {
+	const { id } = request.params;
+	const { title, url, techs }  = request.body;
+
+	const repoIndex = repositories.findIndex(repo => repo.id == id);
+
+	if (repoIndex < 0) {
+		return response.status(400).json({ error: 'Repository not found.'});
+	}
+
+	const repository = { 
+		id, 
+		title, 
+		url, 
+		techs,
+		likes: repositories[repoIndex].likes 
+	};
+
+	repositories[repoIndex] = repository;
+
+	return response.json(repository);
+});
+
+// Delete
+app.delete("/repositories/:id", (request, response) => {
+	const { id } = request.params;
+
+	const repoIndex = repositories.findIndex(repo => repo.id == id);
+
+	if (repoIndex < 0) {
+		return response.status(400).json({ error: 'Repository not found.'});
+	}
+
+	repositories.splice(repoIndex, 1);
+
+	return response.status(204).send();
+});
+
+// Like
+app.post("/repositories/:id/like", (request, response) => {
+	const { id } = request.params;
+
+	const repoIndex = repositories.findIndex(repo => repo.id == id);
+
+	if (repoIndex < 0) {
+		return response.status(400).json({ error: 'Repository not found.'});
+	}
+
+	repositories[repoIndex].likes++;
+
+	return response.json(repositories[repoIndex]);
+});
+
+module.exports = app;
